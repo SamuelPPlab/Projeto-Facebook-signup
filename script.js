34e1910babd3a65ace7bc2d247d077e9d8d5a8b0
rfreitasbatista <rfreitasbatista@gmail.com>
2020-06-04 20:46:20 -0300

    Primeiro commit

---
+++ script.js
@@ -0,0 +1,51 @@
+/*
+let selectDay = document.getElementsByClassName("day")[0];
+
+function days() {
+  for (let i = 0; i < 31; i += 1) {
+    let days = document.createElement("option");
+    days.innerHTML = i + 1;
+    selectDay.appendChild(days);
+  }
+}
+days();
+
+let selectMonth = document.getElementsByClassName("month")[0];
+let monthArray = [
+  "Jan",
+  "Fev",
+  "Mar",
+  "Abr",
+  "Maio",
+  "Jun",
+  "Jul",
+  "Ago",
+  "Set",
+  "Out",
+  "Nov",
+  "Dez",
+];
+
+function months() {
+  for (let i = 0; i < monthArray.length; i += 1) {
+    let months = document.createElement("option");
+    months.innerHTML = monthArray[i];
+    selectMonth.appendChild(months);
+  }
+}
+
+months();
+
+let selectYear = document.getElementsByClassName("year")[0];
+
+function years() {
+  for (let i = 2020; i > 1904; i -= 1) {
+    let years = document.createElement("option");
+    years.innerHTML = i;
+    selectYear.appendChild(years);
+  }
+}
+*/
+//years();
+
+let 
