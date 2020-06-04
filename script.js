92d19420a0af014aac62a35bc967d8124739cf4e
Erlingsjunior <erlingsjunior@gmail.com>
2020-06-04 17:45:25 -0300

    commit de btn javascript

---
+++ script.js
@@ -0,0 +1,5 @@
+const btn = document.querySelector('#button-login');
+btn.addEventListener('click', function () {
+  const email = document.querySelector('#user-email-phone').value;
+  alert(email);
+});
