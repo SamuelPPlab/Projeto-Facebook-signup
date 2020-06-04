bda5dfd01633c4e9afcb9773d3e6e48a77c8bcf5
Carla Nakajuni <carlanakajuni@hotmail.com>
2020-06-04 15:59:05 -0300

    Criando o evento alert no botão de login

---
+++ script.js
@@ -0,0 +1,6 @@
+const buttonLogin = document.getElementById('button-login');
+
+buttonLogin.addEventListener('click', function () {
+  const userEmailPhone = document.getElementById('user-email-phone');
+  alert(userEmailPhone.value);
+});
