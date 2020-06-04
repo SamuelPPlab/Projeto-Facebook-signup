d75d2f2a6b36179e17be07787b19f1807c03b590
matambrosio <matmagamb1@gmail.com>
2020-06-04 17:59:27 -0300

    13º push

--- script.js
+++ script.js
@@ -4,3 +4,4 @@ button1.addEventListener('click', function () {
   const login = document.querySelector('#user-email-phone').value;
   alert(login);
 });
+document.querySelector('.data').DatePickerX.init({ format: 'dd/mm/yyyy' });
