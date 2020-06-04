34f4cd9f249f7a6d45c9a62457cf0e724d9eae7e
Carla Nakajuni <carlanakajuni@hotmail.com>
2020-06-04 17:51:37 -0300

    Adicionando o DatePicker

--- script.js
+++ script.js
@@ -4,3 +4,7 @@ buttonLogin.addEventListener('click', function () {
   const userEmailPhone = document.getElementById('user-email-phone');
   alert(userEmailPhone.value);
 });
+
+const birthDate = document.getElementById('birthdate');
+
+birthDate.DatePickerX.init({format:"dd/mm/yyyy"});
