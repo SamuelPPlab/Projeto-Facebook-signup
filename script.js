a82e84b3b5bea953c796fe09aa201fe18fbd1699
Elisa <elisatcamillo@gmail.com>
2020-06-05 13:09:36 -0300

    trying to implement pikaday

--- script.js
+++ script.js
@@ -6,3 +6,24 @@ function alerta() {
 }
 
 submitButton.addEventListener('click', alerta);
+
+const data = document.getElementById('data');
+
+const picker = new Pikaday({
+  field: document.getElementById('data'),
+  format: 'D/M/YYYY',
+  toString(date, format) {
+    const day = date.getDate();
+    const month = date.getMonth() + 1;
+    const year = date.getFullYear();
+    return `${day}/${month}/${year}`;
+  },
+  parse(dateString, format) {
+    // dateString is the result of `toString` method
+    const parts = dateString.split('/');
+    const day = parseInt(parts[0], 10);
+    const month = parseInt(parts[1], 10) - 1;
+    const year = parseInt(parts[2], 10);
+    return new Date(year, month, day);
+  },
+});
