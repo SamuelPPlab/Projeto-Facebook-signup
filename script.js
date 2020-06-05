ace3799c57910833ea04dc9b09fbf2f3bfa11326
Christian Duessel <duessel@gmail.com>
2020-06-05 14:55:04 -0300

    fix email abaixo sobrenome

--- script.js
+++ script.js
@@ -3,7 +3,6 @@ const login = document.getElementById('button-login');
 login.addEventListener('click', function () {
   const enviarEmail = document.getElementById('user-email-phone').value;
   if (enviarEmail !== 'undefined') {
-    alert('enviarEmail');
+    alert(enviarEmail);
   }
-  alert('Preencha os seus dados');
-});
+  });
