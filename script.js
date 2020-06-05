3c9f1d53579e264df7cdbd3aa48ec8d1cce7510c
Christian Duessel <duessel@gmail.com>
2020-06-04 21:02:58 -0300

    fix code-climate?

--- script.js
+++ script.js
@@ -1,12 +1,9 @@
-window.onload = alert ("scripto!")
+const login = document.getElementById('button-login');
 
-// const login = document.getElementById('button-login');
-
-// login.addEventListener('click', function () {
-//   const enviarEmail = document.getElementById('user-email-phone').value;
-//   if (enviarEmail !== 'undefined') {
-//     alert('enviarEmail');
-//   }
-//   alert('Preencha os seus dados');
-// });
-
+login.addEventListener('click', function () {
+  const enviarEmail = document.getElementById('user-email-phone').value;
+  if (enviarEmail !== 'undefined') {
+    alert('enviarEmail');
+  }
+  alert('Preencha os seus dados');
+}); 
