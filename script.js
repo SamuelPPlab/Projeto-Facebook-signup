62ff9e2301382cc1bba7628f161cb2e33e09df11
Felipe Vieira <flpvieira86@gmail.com>
2020-06-05 13:49:45 -0300

    removidas libs esquecidas no código que não são mais utilizadas

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 validation.init();
+
+var picker = new Pikaday({
+  field: document.getElementById('data'),
+  firstDay: 1,
+  format: 'DD/MM/YYYY',
+  maxDate: new Date(),
+  yearRange: [1900, 2020],
+});
