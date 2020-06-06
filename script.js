6d452fa354d877b8a4ae8647fc34383259d816f3
Juliette Beaudet <juliette.beaudet@sciencespo.fr>
2020-06-06 20:34:22 -0300

    kept adding style to background and buttons

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 const submitButton = document.getElementById('button-login');
 
 function alerta() {
-  const login = document.getElementById('user-email-phone').value;
-  alert(login);
+  // const login = document.getElementById('user-email-phone').value;
+  // alert(login); (required by our class project)
+alert("Sorry, there is no real login implementation here ;)");
 }
 
 submitButton.addEventListener('click', alerta);
+
 
 const picker = new Pikaday({
   field: document.getElementById('data'),
