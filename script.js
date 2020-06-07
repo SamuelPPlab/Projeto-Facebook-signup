37342a3a7f03a4ed288e79defae7743c015b44dd
Juliette Beaudet <juliette.beaudet@sciencespo.fr>
2020-06-06 23:23:10 -0300

    added validation lib for form inputs

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-const submitButton = document.getElementById('button-login');
+validation.init();
 
+const submitButton = document.getElementById('button-login');
 function alerta() {
   // const login = document.getElementById('user-email-phone').value;
   // alert(login); (required by our class project)
   alert('Sorry, there is no real login implementation here!');
 }
-
 submitButton.addEventListener('click', alerta);
 
 // const picker = new Pikaday({
