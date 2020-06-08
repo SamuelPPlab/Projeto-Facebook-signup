4a151d92784a3bd8280ee299f40edd5b99e37c68
Felipe Vieira <flpvieira86@gmail.com>
2020-06-08 13:53:23 -0300

    fix no script de acordo com o gordin

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-// legalllll
+
+/* global validation */
+
+validation.init();
+
+new Pikaday({
+  field: document.getElementById('data'),
+  firstDay: 1,
+  format: 'DD/MM/YYYY',
+  maxDate: new Date(),
+  yearRange: [1900, 2020],
+});
