e5cc11e26791ac520785b11e1f601eb39c757df5
Felipe Vieira <flpvieira86@gmail.com>
2020-06-08 13:25:01 -0300

    ...

--- script.js
+++ script.js
@@ -1,9 +1,1 @@
-validation.init();
-
-var picker = new Pikaday({
-  field: document.getElementById('data'),
-  firstDay: 1,
-  format: 'DD/MM/YYYY',
-  maxDate: new Date(),
-  yearRange: [1900, 2020],
-});
+// legalllll
