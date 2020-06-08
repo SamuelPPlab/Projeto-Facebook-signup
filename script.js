d2c360814a1f109e5ce534e862c1818b60e61d6e
Juliette Beaudet <juliette.beaudet@sciencespo.fr>
2020-06-08 09:11:22 -0300

    reestablished alert js on signin button

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 validation.init();
 
+const submitButton = document.getElementById('button-login');
+function alerta() {
+  // const login = document.getElementById('user-email-phone').value;
+  // alert(login); (required by our class project)
+  alert('Sorry, there is no real login implementation here!');
+}
+submitButton.addEventListener('click', alerta);
+
 const picker = new Pikaday({
   field: document.getElementById('data'),
   firstDay: 1,
@@ -9,11 +17,3 @@ const picker = new Pikaday({
 });
 const formDate = document.getElementById('data');
 formDate.DatePickerX.init({ format: 'dd/mm/yyyy' });
-
-// const submitButton = document.getElementById('button-login');
-// function alerta() {
-//   // const login = document.getElementById('user-email-phone').value;
-//   // alert(login); (required by our class project)
-//   alert('Sorry, there is no real login implementation here!');
-// }
-// submitButton.addEventListener('click', alerta);
