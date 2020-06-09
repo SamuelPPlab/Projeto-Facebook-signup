aff2cad837b62175d7bfe3f1b7942be527843407
Juliette Beaudet <juliette.beaudet@sciencespo.fr>
2020-06-09 14:54:22 -0300

    reestablishing function button-login creating alert

--- script.js
+++ script.js
@@ -4,9 +4,8 @@ validation.init();
 
 const submitButton = document.getElementById('button-login');
 function alerta() {
-  // const login = document.getElementById('user-email-phone').value;
-  // alert(login); (required by our class project)
-  alert('Sorry, there is no real login implementation here!');
+  const login = document.getElementById('user-email-phone').value;
+  alert(login);
 }
 submitButton.addEventListener('click', alerta);
 
