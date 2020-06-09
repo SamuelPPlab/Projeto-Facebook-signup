41aacf17ec9034fde8d5595ee3f1a711c0c071fb
Lucas Costa Ventura <lucascv333@gmail.com>
2020-06-09 11:57:13 -0300

    add verificacao do botao cadastre-se

--- script.js
+++ script.js
@@ -3,3 +3,19 @@ btn.addEventListener('click', function () {
   const email = document.querySelector('#user-email-phone').value;
   alert(email);
 });
+
+const btn2 = document.querySelector('#facebook-register');
+btn2.addEventListener('click', function () {
+  const entradas = document.querySelectorAll('input');
+  let alerta = true;
+  for (let i = 2; i < entradas.length; i += 1) {
+    if (entradas[i].value === '') {
+      alerta = false;
+      alert('Dados Inválidos');
+      return
+    }
+  }
+  if (alerta === true) {
+    alert(`${entradas[2].value} ${entradas[3].value}`);
+  }
+});
