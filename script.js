fd63966fd35cc7ed2c3a97bcf67668f668f90451
Elisa <elisatcamillo@gmail.com>
2020-06-09 11:47:26 -0300

    re-adding pikaday

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const submitButton = document.getElementById('button-login');
 
+const data = document.getElementById('data');
+
 function alerta() {
   const login = document.getElementById('user-email-phone').value;
   alert(login);
@@ -11,15 +13,15 @@ const picker = new Pikaday({
   field: document.getElementById('data'),
   format: 'D/M/YYYY',
   toString(date, format) {
-      // you should do formatting based on the passed format,
-      // but we will just return 'D/M/YYYY' for simplicity
+    // you should do formatting based on the passed format,
+    // but we will just return 'D/M/YYYY' for simplicity
     const day = date.getDate();
     const month = date.getMonth() + 1;
     const year = date.getFullYear();
     return `${day}/${month}/${year}`;
   },
   parse(dateString, format) {
-      // dateString is the result of `toString` method
+    // dateString is the result of `toString` method
     const parts = dateString.split('/');
     const day = parseInt(parts[0], 10);
     const month = parseInt(parts[1], 10) - 1;
