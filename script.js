0e9038adade668cbe5058f11c630ce9eb77875c2
marinabrcls <barcelos.marina@gmail.com>
2020-06-10 18:37:46 -0300

    Tentando arrumar os inputs senha e data de nascimento

--- script.js
+++ script.js
@@ -1,11 +1,8 @@
-// Variáveis
 const loginButton = document.getElementById('button-login');
 const emailInput = document.getElementById('user-email-phone');
 
-// Funções
 function alertaLogin() {
   alert(emailInput.value);
 }
 
-// Event Listeners
 loginButton.addEventListener('click', alertaLogin);
